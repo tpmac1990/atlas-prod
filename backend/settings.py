795bbf7f20babd1429a5766a87f72bd12fd7ac83
tpmac1990 <terence.paulmac@gmail.com>
2021-12-31 08:50:08 +1100

    added production SECRET_KEY path

--- backend/settings.py
+++ backend/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('AT_SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = BASE_DIR == 'C:\\Django_Projects\\03_geodjango\\Atlas\\atlas'
+
+SECRET_KEY = os.environ.get('AT_SECRET_KEY') if DEBUG else os.environ['SECRET_KEY']
 
 # ‘*’ will allow any domain to host the page. Once the app is deployed, only the domains you want to use here should be listed for security reasons.
 ALLOWED_HOSTS = ['*']
